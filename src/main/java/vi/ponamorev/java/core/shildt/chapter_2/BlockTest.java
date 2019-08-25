56272b3d0d2960f0e35236978e733973235eea01
Ponamorev <penzayk@mail.ru>
2019-08-25 21:43:03 +0300

    Shildt. Chapter 2. Finish

---
+++ src/main/java/vi/ponamorev/java/core/shildt/chapter_2/BlockTest.java
@@ -0,0 +1,21 @@
+package vi.ponamorev.java.core.shildt.chapter_2;
+
+/*
+    Show using code blocks
+
+    Set source file name 'BlockTest.java'
+ */
+public class BlockTest {
+    public static void main(String[] args) {
+        int x, y;
+
+        y = 20;
+
+        // recipient of the loop is that code block
+        for (x = 0; x < 10; x++) {
+            System.out.println("Value of 'x': " + x);
+            System.out.println("Value of 'y': " + y);
+            y = y - 2;
+        }
+    }
+}
---
+++ src/main/java/vi/ponamorev/java/core/shildt/chapter_2/ForTest.java
@@ -0,0 +1,15 @@
+package vi.ponamorev.java.core.shildt.chapter_2;
+
+/*
+    Show the loop 'for' working.
+
+    Set source file name 'ForTest.java'
+ */
+class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x++)
+            System.out.println("Value of 'x': " + x);
+    }
+}
